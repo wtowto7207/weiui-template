53e6cd260cea1c129a126e81e37df2b85db410c8
kuaifan <aipaw@live.cn>
2018-09-23 12:39:40 +0800

    no message

--- weiui.config.js
+++ weiui.config.js
@@ -2,6 +2,9 @@ module.exports = {
 
     homePage: "",   //主页的JS地址（build使用，留空自动生成）
 
+    appkey: "3TRWyttKzxRBtmHc004sJMjjyAxOf08l",     //（可选）用于云平台管理
+    version: "1.0.0",                               //（可选）用于云平台热更新
+
     rongim: {       //融云模块配置
         ios: {
             enabled: false,
